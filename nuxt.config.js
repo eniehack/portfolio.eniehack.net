a2ae73e8a93d29ec278f40de1d42c188cbd663a7
caffeine <eniehack@outlook.jp>
2019-10-06 13:45:26 +0900

    init commit

---
+++ nuxt.config.js
@@ -0,0 +1,66 @@
+const pkg = require('./package')
+
+module.exports = {
+  mode: 'universal',
+
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: pkg.name,
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "stylesheet", href: "https://cdn.jsdelivr.net/npm/fork-awesome@1.1.7/css/fork-awesome.min.css", integrity: "sha256-gsmEoJAws/Kd3CjuOQzLie5Q3yshhvmo7YNtBG7aaEY=", crossorigin: "anonymous"}
+    ]
+  },
+
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#FFFFFF' },
+
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    "~plugins/buefy"
+  ],
+
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    ['nuxt-buefy', { css: true, materialDesignIcons: false }]
+  ],
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,85 @@
+<template>
+
+  <section class="container">
+    <div class="columns is-variable is-8-desktop">
+      <div class="has-text-right introduction">
+        <figure class="image is-square" title="avatar">
+          <img src="avatar.svg" alt="avatar" />
+        </figure>
+        <div class="">
+          <h1 class="title is-size-1-desktop">
+            Nakaya
+          </h1>
+          <h2 class="subtitle is-size-2-desktop">
+            a Web developer
+          </h2>
+          <div class="links columns is-mobile is-multiline is-variable is-1-desktop">
+            <a href="https://twitter.com/eniehack" rel="noopener me" target="_blank" class="column" title="twitter">
+              <i class="fa fa-twitter fa-3x" aria-hidden="true" />
+            </a>
+            <a href="https://github.com/eniehack" rel="noopener me" target="_blank" class="column" title="github">
+              <i class="fa fa-github fa-3x" aria-hidden="true" />
+            </a>
+            <a href="https://keybase.io/eniehack" rel="noopener me" target="_blank" class="column" title="keybase">
+              <i class="fa fa-keybase fa-3x" aria-hidden="true" />
+            </a>
+            <a href="https://pleroma.eniehack.net/eniehack" rel="noopener me" target="_blank" class="column" title="Pleroma">
+              <i class="fa fa-pleroma fa-3x" aria-hidden="true" />
+            </a>
+            <a href="https://matrix.to/#/@eniehack:matrix-jp.net" rel="noopener me" target="_blank" class="column" title="Matrix">
+              <i class="fa fa-matrix-org fa-3x" aria-hidden="true" />
+            </a>
+            <a href="xmpp:eniehack@jabber.at" rel="noopener me" target="_blank" class="column" title="Pleroma">
+              <i class="fa fa-xmpp fa-3x" aria-hidden="true" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="content column is-one-three-desktop">
+        <div class="tabs">
+          <b-tabs>
+            <b-tab-item label="About">
+                <h1 class="title is-size-2-desktop" id="About">About</h1>
+                <p>非情報系高専生です。</p>
+                <p>気の赴くままにコードを書いています。</p>
+                <p>普段はWebバックエンドをやっていますが、たまにフロントエンドやArduinoをいじっています。</p>
+            </b-tab-item>
+
+            <b-tab-item label="Interest">
+              <h1 class="title is-size-2-desktop" id="interest">Interest</h1>
+              <h2 class="has-text-weight-light is-size-3-desktop">Decenterlized Network</h2>
+              <h2 class="has-text-weight-light is-size-3-desktop">Document format(Markup language)</h2>
+            </b-tab-item>
+
+            <b-tab-item label="Like">
+              <h1 class="title is-size-2-desktop" id="like">Like</h1>
+              <h2 class="has-text-weight-light is-size-3-desktop">Functional Programming</h2>
+              <h2 class="has-text-weight-light is-size-3-desktop">Free Software(Open Source Software)</h2>
+              <h2 class="has-text-weight-light is-size-3-desktop">ArchLinux</h2>
+              <h2 class="has-text-weight-light is-size-3-desktop">Visual Novel</h2>
+            </b-tab-item>
+
+            <b-tab-item label="Skills">
+              <h1 class="title is-size-2-desktop" id="skills">Skills</h1>
+              <h2 class="has-text-weight-light is-size-3-desktop">Golang</h2>
+              <h2 class="has-text-weight-light is-size-3-desktop">ESP32</h2>
+              <h2 class="has-text-weight-light is-size-3-desktop">Arduino</h2>
+              <h2 class="has-text-weight-light is-size-3-desktop">Linuxサーバ運用</h2>
+            </b-tab-item>
+            <b-tab-item label="Works">
+              <h1 class="title is-size-2-desktop" id="works">Works</h1>
+              <h2 class="has-text-weight-light is-size-3-desktop">Golang</h2>
+              <h2 class="has-text-weight-light is-size-3-desktop">ESP32</h2>
+              <h2 class="has-text-weight-light is-size-3-desktop">Arduino</h2>
+            </b-tab-item>
+            <b-tab-item label="Links">
+              <h1 class="title is-size-2-desktop" id="links">Links</h1>
+              <p>Misskey</p>
+              <p>Blog</p>
+            </b-tab-item>
+          </b-tabs>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
