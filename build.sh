2aad11069468023127a101837ffc07f82da0c797
Nakaya <eniehack@outlook.jp>
2022-11-07 16:06:11 +0900

    feature: delete for exists file

--- build.sh
+++ build.sh
@@ -2,7 +2,15 @@
 
 set -eu
 
+if [ -e "foaf.xml" ]; then
+    rm "foaf.xml"
+fi
+if [ -e "build" ] && [ -d "build" ]; then
+    rm -r "build"
+fi
+
 mkdir -p ./build
+
 cat foaf.template.xml \
     | sed \
         "s/<dcterms:modified rdf:datatype=\"xs:dateTime\">/&$(date -Isecond)/"
