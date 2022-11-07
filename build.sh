8ad7e3a88642a0fceb8d7db09643e0c538fd5c98
Nakaya <eniehack@outlook.jp>
2022-11-07 15:51:00 +0900

    feature: build script

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 set -euo
 
 mkdir -p ./build
+cat foaf.template.xml \
+    | sed \
+        "s/<dcterms:modified rdf:datatype=\"xs:dateTime\">/&$(date -Isecond)/"
+        > foaf.xml
+
 xsltproc -o ./build/index.html html.xsl foaf.xml
 cp -r ./assets ./build
 cp -r foaf.xml ./build
